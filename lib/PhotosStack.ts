6fbde038ed81bc609340959bac67762ca5ed835f
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-06-29 08:34:23 +0530

    feat: intrinsic function

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -1,20 +1,22 @@
 import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
 import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Fn } from "aws-cdk-lib";
 
 export class PhotosStack extends cdk.Stack {
+  private stackSuffix: string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new Bucket(this, "PhotosBucket-abhimanranaweera2", {
-      bucketName: "photosbucketabhimanranaweera123",
+    this.initializeSuffix();
+
+    new Bucket(this, "PhotosBucketAbhiman", {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
     });
+  }
 
-    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId(
-      "photosbucketabhimanranaweera456"
-    );
-
-    // create a new resource
-    // delete the old one
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split("/", this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split("-", shortStackId));
   }
 }
