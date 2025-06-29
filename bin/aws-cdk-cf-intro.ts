d9094686135fb21f3c2cc20a954c85d180d204ee
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-06-29 09:24:13 +0530

    chore: use the photo Handler stack

--- bin/aws-cdk-cf-intro.ts
+++ bin/aws-cdk-cf-intro.ts
@@ -3,7 +3,9 @@ import * as cdk from "aws-cdk-lib";
 //import { AwsCdkCfIntroStack } from "../lib/aws-cdk-cf-intro-stack";
 import { PhotosStack } from "../lib/photosStack";
 import { Bucket } from "aws-cdk-lib/aws-s3";
+import { PhotosHandlerStack } from "../lib/PhotosHandlerStack";
 
 const app = new cdk.App();
 //new AwsCdkCfIntroStack(app, "AwsCdkCfIntroStack");
 new PhotosStack(app, "PhotosStack");
+new PhotosHandlerStack(app, "PhotosHandlerStack");
