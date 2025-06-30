ad2a6aa3eab98bcd6f18f53fa4a58963bf413e85
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-06-30 07:49:57 +0530

    feat: add tags to resources

--- bin/aws-cdk-cf-intro.ts
+++ bin/aws-cdk-cf-intro.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 //import { AwsCdkCfIntroStack } from "../lib/aws-cdk-cf-intro-stack";
-import { PhotosStack } from "../lib/photosStack";
-import { Bucket } from "aws-cdk-lib/aws-s3";
 import { PhotosHandlerStack } from "../lib/PhotosHandlerStack";
+import { PhotosStack } from "../lib/PhotosStack";
+import { BucketTagger } from "./Tagger";
 
 const app = new cdk.App();
 //new AwsCdkCfIntroStack(app, "AwsCdkCfIntroStack");
 new PhotosStack(app, "PhotosStack");
 new PhotosHandlerStack(app, "PhotosHandlerStack");
+const tagger = new BucketTagger("level", "test");
+cdk.Aspects.of(app).add(tagger);
